780146a8a2cd282174cadb2786345ff4a8daae00
lores <lore.sarti.93@gmail.com>
2021-02-17 19:03:43 +0100

    Created project

---
+++ src/main/java/edu/att4sd/SensorReaderApplication.java
@@ -0,0 +1,13 @@
+package edu.att4sd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorReaderApplication.class, args);
+	}
+
+}
