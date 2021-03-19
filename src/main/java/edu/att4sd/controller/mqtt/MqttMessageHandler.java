73ff876f935d8daf81373e910f718fe58c699b77
lores <lore.sarti.93@gmail.com>
2021-03-19 12:28:45 +0100

    Isolated MqttMessageHandlerTest
    Previously were too coupled with the receiver beans

--- src/main/java/edu/att4sd/controller/mqtt/MqttMessageHandler.java
+++ src/main/java/edu/att4sd/controller/mqtt/MqttMessageHandler.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.mqtt.support.MqttHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.stereotype.Component;
@@ -22,7 +23,7 @@ public class MqttMessageHandler implements MessageHandler{
 
 	@Override
 	public void handleMessage(Message<?> message) {
-		String topic = message.getHeaders().get("mqtt_receivedTopic").toString();
+		String topic = message.getHeaders().get(MqttHeaders.RECEIVED_TOPIC).toString();
 		String value = message.getPayload().toString();
 		TelemetryValue received = new TelemetryValue(Instant.now(), value);
 		logger.debug("["+topic+"] - "+received.toString());
--- src/test/java/edu/att4sd/controller/mqtt/MqttMessageHandlerTest.java
+++ src/test/java/edu/att4sd/controller/mqtt/MqttMessageHandlerTest.java
@@ -4,31 +4,26 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 
-import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.integration.mqtt.support.MqttHeaders;
+import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import edu.att4sd.model.TelemetryValue;
 import edu.att4sd.services.TopicService;
 
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {MqttMessageHandlerConfig.class, MqttReceiverConfig.class})
+@ExtendWith(MockitoExtension.class)
 class MqttMessageHandlerTest {
 	
-	@MockBean
+	@Mock
 	private TopicService service;
-	
-	@Autowired
-	private DefaultPahoMessageConverter mqttMessageConverter;
-	
-	@Autowired
+		
+	@InjectMocks
 	private MqttMessageHandler mqttMsgHandler;
 	
 
@@ -36,11 +31,12 @@ class MqttMessageHandlerTest {
 	void testHandleMessage() {
 		String topic = "/test/topic";
 		String value = "TestMessage";
-		MqttMessage message = new MqttMessage(value.getBytes());
-		Message<?> converted = mqttMessageConverter.toMessage(topic, message);
+		Message<?> message = MessageBuilder.withPayload(value)
+										   .setHeader(MqttHeaders.RECEIVED_TOPIC, topic)
+										   .build();
 		doNothing().when(service).addTelemetryValue(any(String.class), any(TelemetryValue.class));
 		
-		mqttMsgHandler.handleMessage(converted);
+		mqttMsgHandler.handleMessage(message);
 		
 		verify(service).addTelemetryValue(any(String.class), any(TelemetryValue.class));
 	}
