c1fa53befb74e408a00fa6b24fd9bc48f903cc6d
lores <lore.sarti.93@gmail.com>
2021-03-24 12:41:53 +0100

    Updated Topic Model to support no-args constructor

--- src/main/java/edu/att4sd/model/Topic.java
+++ src/main/java/edu/att4sd/model/Topic.java
@@ -9,6 +9,8 @@ public class Topic {
 	private String id;
 	private String path;
 	private List<TelemetryValue> telemetry;
+	
+	public Topic() { }
 
 	public Topic(String path, List<TelemetryValue> telemetry) {
 		this.path = path;
@@ -37,8 +39,15 @@ public class Topic {
 
 	@Override
 	public String toString() {
-		return "[" + path + "]:{" +
-				telemetry.stream().map(Object::toString).collect(Collectors.joining(", ")) + "}";
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		sb.append(path == null ? "-" : path);
+		sb.append("]:{");
+		sb.append(telemetry == null ? "-" : telemetry.stream()
+													 .map(Object::toString)
+													 .collect(Collectors.joining(", ")));
+		sb.append("}");
+		return sb.toString();
 	}
 
 	@Override
